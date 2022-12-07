09664f7515ce257e012f7fbae8ad2f1e08b5a0ee
Wesley Seubring <w.seubring@researchable.nl>
2022-12-07 12:21:38 +0100

    code: day 6

---
+++ 6/b/index.ts
@@ -0,0 +1,28 @@
+import * as fs from "fs";
+import _ from "lodash";
+
+const getUniqueMarkerIndex = (line: string) => {
+  let idx = 0;
+  const chunkSize = 14;
+  while (idx < line.length - chunkSize) {
+    const chunk = line.slice(idx, idx + chunkSize);
+    if (_.uniq(chunk).length === chunkSize) {
+      return idx + chunkSize;
+    }
+    idx += 1;
+  }
+  return -1;
+};
+
+const result = () => {
+  const path = "6/input";
+  const file = fs.readFileSync(path, "utf8");
+  const lines = file.split("\n");
+  const line = lines[0];
+
+  const uniqueMarkerIndex = getUniqueMarkerIndex(line);
+
+  return uniqueMarkerIndex;
+};
+
+console.log(result());
